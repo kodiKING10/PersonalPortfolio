e868d49bc868d06e7d84a2333143ee1e5847d13f
Henrique Cavalcante <hencavalcante99@gmail.com>
2020-05-03 01:21:24 -0300

    Contact page

--- src/pages/Contacts/index.js
+++ src/pages/Contacts/index.js
@@ -9,6 +9,74 @@ export default function Contacts(){
         <div className="container">
             <Header />
 
+            <div className="screenPath"><p className="screenPathText">Home / Contacts</p></div>
+            <div className="wrapper">
+                <div className="wrapperContact">
+
+                    <div className="boxContact">
+
+                        <div className="infoContact">
+
+                            <div className="iconTel"></div>
+
+                            <div className="textContact">
+                                <p className="titleContact">Cell Phone</p>
+                                <p className="descriptionContact">+55 (11) 98237-1115</p>
+                            </div>
+
+                        </div>
+
+                    </div>
+                    <div className="boxContact-lower">
+
+                        <div className="infoContact">
+
+                            <div className="iconAddress"></div>
+
+                            <div className="textContact">
+                                <p className="titleContact">Address</p>
+                                <p className="descriptionContact">São Paulo - SP , Brazil</p>
+                            </div>
+
+                        </div>
+
+                    </div>
+
+                </div>  
+
+                <div className="wrapperContact">
+                    <div className="boxContact">
+
+                        <div className="infoContact">
+
+                            <div className="iconEmail"></div>
+
+                            <div className="textContact">
+                                <p className="titleContact">E-mail</p>
+                                <p className="descriptionContact">hencavalcante99@gmail.com</p>
+                            </div>
+
+                        </div>
+
+                    </div>
+
+                    <div className="boxContact-lower">
+
+                        <div className="infoContact">
+
+                            <div className="iconEmail"></div>
+
+                            <div className="textContact">
+                                <p className="titleContact">LinkedIn</p>
+                                <p className="descriptionContact">Henrique Cavalcante Veiga</p>
+                            </div>
+
+                        </div>
+
+                    </div>
+                </div>
+            </div>    
+            
             <Footer />
         </div>
     );
