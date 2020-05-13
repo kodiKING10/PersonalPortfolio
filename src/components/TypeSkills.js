dc6362e02f5dfdaaa7208b326f3cb426a21975d3
Henrique Cavalcante <hencavalcante99@gmail.com>
2020-05-13 20:41:47 -0300

    type skills component fix

--- src/components/TypeSkills.js
+++ src/components/TypeSkills.js
@@ -5,7 +5,7 @@ export default function TypeSkills(){
     return(
         <ReactTypingEffect
             text={['C#', '.NET Core', 'SQL Server', 'Javascript', 'Node.js', 'React.js']}
-            loop="true"
+            loop={true}
             speed="180"
             eraseDelay="2500"
             typingDelay="2000"
