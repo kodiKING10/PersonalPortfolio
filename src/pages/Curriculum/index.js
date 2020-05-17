24b4e88d7f98443e52bc4779e88a6f96721cba21
Henrique Cavalcante <hencavalcante99@gmail.com>
2020-05-17 13:09:22 -0300

    curriculum page start

--- src/pages/Curriculum/index.js
+++ src/pages/Curriculum/index.js
@@ -1,14 +1,10 @@
 import React from 'react';
-
 import './styles.css';
-import Header from '../../components/Header.js';
-import Footer from '../../components/Footer.js';
 
 export default function Curriculum(){
     return (
         <div className="container">
-            <Header />
-            <Footer />
+            
         </div>
     );
 }
