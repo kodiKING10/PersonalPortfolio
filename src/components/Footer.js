503fb60e944824405237717a0a8bef2088dfbb42
Henrique Cavalcante <hencavalcante99@gmail.com>
2020-06-17 20:31:29 -0300

    Origin

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,30 +1,9 @@
 import React from 'react';
-import imgGooglePlus from '../../src/assets/google-plus.png';
-import imgLinkedin from '../../src/assets/linkedin.png';
-import imgWhatsapp from '../../src/assets/whatsapp.png';
-import { Link } from 'react-router-dom';
 
 function Footer(){
     return (
         <footer>
-            <div className="footerEmailTel">
-                    <p className="email">hencavalcante99@gmail.com</p>
-                    <p className="telefone">+55 (11) 98237-1115</p>
-            </div>
-
-            <div className="footerSocialMedia">
-
-                <div className="footerTextSocialMedia">
-                    <p>Social Media<span className="indicator"></span></p>
-                </div>    
-
-                <div className="imgSocialMedia">
-                    <Link to="/contacts" ><img  src={imgWhatsapp} alt="w"/></Link>
-                    <Link to="/contacts" ><img  src={imgLinkedin} alt="L"/></Link>
-                    <Link to="/contacts" ><img  src={imgGooglePlus} alt="G"/></Link>
-                </div>
-
-            </div>
+           
         </footer>
     );
 }
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,27 +1,12 @@
 import React from 'react';
 import './styles.css';
-import Header from '../../components/Header.js';
-import Footer from '../../components/Footer.js';
 import TypeSkills from '../../components/TypeSkills.js';
 
 export default function Home() {
         
     return(
         <div className="container">
-
-            <Header />
-
-            <section>
-                <div className="corpoHome">
-                    <div className="divCentralText">
-                        <p className="centralText">Welcome to my portfolio, I'm <span className="greenCentralText">Henrique Cavalcante Veiga</span> a FullStack Developer Currently Based in Brazil, São Paulo.</p>
-                    </div>
-                </div>
-            </section> 
-
-            <Footer />
-
-            <div className="caixaSkills">
+            <div>
                 <TypeSkills />
             </div>  
         </div>         
