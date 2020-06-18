8f5c5c7f2e6f16c00d87ad9590fd2352e6869fd9
Henrique Cavalcante <hencavalcante99@gmail.com>
2020-06-18 19:34:49 -0300

    kick-off

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import './styles.css';
+import imgHome from '../../assets/imgHome.jpg';
 
 export default function Home() {
         
     return(
         <div className="container">
+            
+            <img src={imgHome} alt="imgHome" className="imgHome"></img>
             <header className="header">
                 <ul className="listHeader">
                     <li className="listItem-header">About me</li>
@@ -13,7 +16,7 @@ export default function Home() {
                     <li className="listItem-header">Skills</li>
                     <li className="listItem-header">Contact me</li>
                 </ul>
-            </header>  
+            </header>            
         </div>         
     );   
 }
