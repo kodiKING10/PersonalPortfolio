6193543ff91345099494a12fc4c270dceeaabaf8
Henrique Cavalcante <hencavalcante99@gmail.com>
2020-06-28 16:33:47 -0300

    Kick-off front-end

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,22 +1,36 @@
 import React from 'react';
 import './styles.css';
-import imgHome from '../../assets/imgHome.jpg';
 
 export default function Home() {
         
     return(
         <div className="container">
-            
-            <img src={imgHome} alt="imgHome" className="imgHome"></img>
-            <header className="header">
-                <ul className="listHeader">
-                    <li className="listItem-header">About me</li>
-                    <li className="listItem-header">Education</li>
-                    <li className="listItem-header">Experience</li>
-                    <li className="listItem-header">Skills</li>
-                    <li className="listItem-header">Contact me</li>
-                </ul>
-            </header>            
+
+            <section className="sectionHome">        
+                <header className="header">
+                    <ul className="listHeader">
+                        <li className="listItem-header"><a href="#sectionAboutMe">About me</a></li>
+                        <li className="listItem-header"><a href="#sectionAboutMe">Education</a></li>
+                        <li className="listItem-header"><a href="#sectionAboutMe">Experience</a></li>
+                        <li className="listItem-header"><a href="#sectionAboutMe">Skills</a></li>
+                        <li className="listItem-header"><a href="#sectionAboutMe">Contact me</a></li>
+                    </ul>
+                </header>
+
+                <div className="boxHome">
+                    <h6>Hello!</h6>
+                    <h1>I'm Henrique Cavalcante</h1>
+                    <h3>FullStack Web Developer</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo. Lorem ipsum dolor 
+                    sit amet.</p>
+                </div>
+            </section>
+
+            <section className="sectionAboutMe">
+                <div width="10px" height="50px" className="verticalCenteredLine"></div>
+                <h4 id="sectionAboutMe">About me</h4>
+                <h1>My hobbies and interests</h1>
+            </section>            
         </div>         
     );   
 }
