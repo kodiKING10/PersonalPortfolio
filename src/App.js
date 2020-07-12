abd099cb9cff093fbdadda63021abd9ad622cb60
Henrique Cavalcante <hencavalcante99@gmail.com>
2020-07-12 14:08:54 -0300

    implementing ga

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import './global.css'
 import Routes from './routes';
-import ReactGA from 'react-ga';
+// import ReactGA from 'react-ga';
 
-function initializeReactGA() {
-  ReactGA.initialize('UA-172418049-1');
-  ReactGA.pageview('/');
-}
+// function initializeReactGA() {
+//   ReactGA.initialize('UA-172418049-1');
+//   ReactGA.pageview('/');
+// }
 
 function App() {
   return (
