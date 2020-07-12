646c137ab84b6949c0ee56efb1deb44aef6e7873
Henrique Cavalcante <hencavalcante99@gmail.com>
2020-07-12 15:10:06 -0300

    .

--- src/App.js
+++ src/App.js
@@ -1,12 +1,6 @@
 import React from 'react';
 import './global.css'
 import Routes from './routes';
-import ReactGA from 'react-ga';
-
-ReactGA.initialize('UA-172418049-1', {
-  debug: true,
-});
-ReactGA.pageview(window.location.pathname + window.location.search);
 
 function App() {
   return (
