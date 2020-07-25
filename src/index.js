8835d160f4a88b0044563c8f2ac92ad00f28eaf0
Henrique Cavalcante <hencavalcante99@gmail.com>
2020-07-25 14:56:20 -0300

    integrating analytics

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import ReactGA from 'react-ga';
 
 const trackingId = "UA-172418049-1"; // Replace with your Google Analytics tracking ID
 ReactGA.initialize(trackingId);
+ReactGA.pageview("/");
 
 ReactDOM.render(<App />,document.getElementById('root'));
 
