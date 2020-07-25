be683e5b8ff6c27ab96b17dba5310aa955cbae4c
Henrique Cavalcante <hencavalcante99@gmail.com>
2020-07-25 14:59:47 -0300

    implementing analytics

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-
 import ReactGA from 'react-ga';
 
 const trackingId = "UA-172418049-1"; // Replace with your Google Analytics tracking ID
