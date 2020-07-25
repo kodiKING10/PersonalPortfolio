3fa6fea1cd2f8a15e9768e664aa8c1a2e0a0e40a
Henrique Cavalcante <hencavalcante99@gmail.com>
2020-07-25 14:51:02 -0300

    .

--- src/index.js
+++ src/index.js
@@ -2,5 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+import ReactGA from 'react-ga';
+
+const trackingId = "UA-172418049-1"; // Replace with your Google Analytics tracking ID
+ReactGA.initialize(trackingId);
+
 ReactDOM.render(<App />,document.getElementById('root'));
 
